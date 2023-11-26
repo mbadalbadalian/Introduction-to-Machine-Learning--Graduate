a474d47b0a21bf8da3c9bb0722e4c6dd136e04be
Chris Binoi Verghese <chris.verghese@hotmail.com>
2023-11-26 01:03:58 -0500

    Model BERT

--- Code/tester.py
+++ Code/tester.py
@@ -1,1 +1,18 @@
-print('hi')
+from transformers import BertConfig, TFBertModel
+import tensorflow as tf
+# Building the config
+config = BertConfig()
+
+# Building the model from the config
+model = TFBertModel(config)
+sequences = ["Hello!", "Cool.", "Nice!"]
+encoded_sequences = [
+    [101, 7592, 999, 102],
+    [101, 4658, 1012, 102],
+    [101, 3835, 999, 102],
+]
+
+
+model_inputs = tf.constant(encoded_sequences)
+output = model(model_inputs)
+print(output)
